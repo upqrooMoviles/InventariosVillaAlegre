1676a0115c30ac5b1f2566a8b3842ae24108bd28
Mariana V <marivale_0@hotmail.com>
2015-04-02 13:32:59 -0500

    comentarios de clase metodosSQL

--- InventariosVillaAlegre/metodosSQL.cs
+++ InventariosVillaAlegre/metodosSQL.cs
@@ -11,28 +11,61 @@ namespace InventariosVillaAlegre
     {
         conexion con = new conexion();
 
-        
+        /*metodo busqueda
+        *Se tiene que enviar el nombre de la tabla, los campos separados por comas y la clausula o clauslas a buscar
+        *Ejemplo:
+         *string tabla="usuarios";
+         *string campos= "id, nombre"; 
+         *string clausula="nombre='maria' ";
+        */
         public DataSet busqueda(string tabla, string campos, string clausula)
         {
+            
             DataSet resultado;
             if (clausula == null)
                 resultado = con.resultado("SELECT " + campos + " FROM " + tabla);
             else
                 resultado = con.resultado("SELECT " + campos + " fROM " + tabla + " WHERE "+clausula);
-            return resultado;
+            return resultado; //Esto nos retornara un dataset (una tabla no fisica de donde se pueden extraer los datos)
         }
 
+
+
+        /*metodo insertar
+        *Se tiene que enviar el nombre de la tabla, los campos separados por comas y los valores a insertar en los campos con comillas simples si no son datos enteros
+        *Ejemplo:
+         *string tabla="usuarios";
+         *string campos= "id, nombre, usuario"; 
+         *string valores="0, 'mariana', 'mar'";
+        */
         public Boolean insertar(string tabla, string campos, string valores)
         {
-            return con.comando("INSERT INTO "+tabla+" ("+campos+") VALUES ("+valores+");");
+            return con.comando("INSERT INTO "+tabla+" ("+campos+") VALUES ("+valores+");");//retorna false si no se ejecuto
         }
+
+
+        /*metodo eliminar
+        *Se tiene que enviar el nombre de la tabla y la clausula o clausulas para eliminar
+        *Ejemplo:
+         *string tabla="usuarios";
+         *string clausula="nombre='maria' ";
+        */
         public Boolean eliminar(string tabla, string clausula)
         {
-            return con.comando("DELETE FROM " + tabla + " WHERE " + clausula + ";");
+            return con.comando("DELETE FROM " + tabla + " WHERE " + clausula + ";");//retorna false si no se ejecuto
         }
+
+
+        /*metodo modificar
+        *Se tiene que enviar el nombre de la tabla, los valores a modificar y la clausula o clauslas donde se modificara
+        *Ejemplo:
+         *string tabla="usuarios";
+         *string valores= "nombre='marisol' and user='mari01'"; 
+         *string clausula="id=0 ";
+        */
         public Boolean modificar(string tabla, string valores, string clausula)
         {
-            return con.comando("UPDATE " + tabla + " SET " + valores + " WHERE " + clausula + ";");
+            return con.comando("UPDATE " + tabla + " SET " + valores + " WHERE " + clausula + ";");//retorna false si no se ejecuto
             
         }
 
