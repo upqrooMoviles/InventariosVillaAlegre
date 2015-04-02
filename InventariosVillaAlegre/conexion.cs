cdd8649b370cae0c7381665e37c8d162a4219443
Mariana V <marivale_0@hotmail.com>
2015-04-02 13:19:36 -0500

    Metodos MySQL

---
+++ InventariosVillaAlegre/conexion.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace InventariosVillaAlegre
+{
+    class conexion
+    {
+
+
+
+        public MySqlConnection conectar;
+        public void crearconexion()
+        {
+            string conec = "Server=localhost;database=villaalegre;uid=root;pwd=;";
+            conectar = new MySqlConnection(conec);
+            conectar.Open();
+        }
+
+        public void cerrarconexion()
+        {
+            conectar.Close();
+        }
+
+        public MySqlConnection getConexion()
+        {
+            return conectar;
+        }
+
+        public DataSet resultado(string buscar)
+        {
+            DataSet data = new DataSet();
+            crearconexion();
+            MySqlCommand busquedaSearch = new MySqlCommand(buscar, getConexion());
+            MySqlDataAdapter cmc = new MySqlDataAdapter(busquedaSearch);
+            busquedaSearch.Connection = getConexion();
+            cmc.Fill(data);
+            return data;
+        }
+
+
+        public Boolean comando(string actualizar)
+        {
+            crearconexion();
+            try
+            {
+                MySqlCommand Ins = new MySqlCommand(actualizar);
+                Ins.Connection = getConexion();
+                Ins.ExecuteNonQuery();
+                cerrarconexion();
+                return true;
+                
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                cerrarconexion();
+            }
+        }
+
+    }
+}
---
+++ InventariosVillaAlegre/metodosSQL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventariosVillaAlegre
+{
+    class metodosSQL
+    {
+        conexion con = new conexion();
+
+        
+        public DataSet busqueda(string tabla, string campos, string clausula)
+        {
+            DataSet resultado;
+            if (clausula == null)
+                resultado = con.resultado("SELECT " + campos + " FROM " + tabla);
+            else
+                resultado = con.resultado("SELECT " + campos + " fROM " + tabla + " WHERE "+clausula);
+            return resultado;
+        }
+
+        public Boolean insertar(string tabla, string campos, string valores)
+        {
+            return con.comando("INSERT INTO "+tabla+" ("+campos+") VALUES ("+valores+");");
+        }
+        public Boolean eliminar(string tabla, string clausula)
+        {
+            return con.comando("DELETE FROM " + tabla + " WHERE " + clausula + ";");
+        }
+        public Boolean modificar(string tabla, string valores, string clausula)
+        {
+            return con.comando("UPDATE " + tabla + " SET " + valores + " WHERE " + clausula + ";");
+            
+        }
+
+    }
+}
